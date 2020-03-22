ff68bcb33ea43eb5fa98e27ed339c34a74126890
Chris Pettyjohn <cpettyjohn@drivestl.com>
2020-03-22 14:21:39 -0500

    Proper modal implementation

--- web/src/components/generic/modal/modal-overlay/ModalOverlay.tsx
+++ web/src/components/generic/modal/modal-overlay/ModalOverlay.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './ModalOverlay.scss';
 import { ModalOverlayProps } from './';
+import { Modal, ModalBody } from 'reactstrap';
 
 export function ModalOverlay({ children, header }: ModalOverlayProps) {
   return (
-    <div id="login-dialog" className="login-popup zoom-anim-dialog animated fadeIn">
+    <Modal className="login-popup" isOpen={true}>
       <h3 className="aside-title">{header}</h3>
       {children}
-    </div>
+    </Modal>
   );
 }
