f886684faedd66633f291914d2f2e16b37d56580
Chris Pettyjohn <cpettyjohn@drivestl.com>
2020-03-26 23:24:26 -0500

    Prettier

--- interface/build/user/User.d.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: number;
-    username: string;
-    credits: number;
-    pixels: number;
-    online: boolean;
-    figure: string;
-}
-export declare const exampleUser: User;
