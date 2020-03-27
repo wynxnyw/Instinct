bea19bc8b0f5e68c55f709d9e17d4e7175cfdf19
Chris Pettyjohn <cpettyjohn@drivestl.com>
2020-03-26 23:26:39 -0500

    dynamic class type

--- api/src/user/constraint/index.ts
+++ api/src/user/constraint/index.ts
@@ -4,8 +4,9 @@ export * from './unique-email.constraint';
 export * from './user-exists.constraint';
 export * from './unique-username.constraint';
 import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import { Class } from 'fashionkilla-interfaces';
 
 
-export const userConstraints:Validator = [
+export const userConstraints: Array<Class<ValidatorConstraintInterface>> = [
   UniqueEmailConstraint,
 ]
