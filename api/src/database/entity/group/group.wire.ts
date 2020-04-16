04417504b8e896f2a79cc63f0890b55faa2c0871
chrismpettyjohn <cpettyjohn@drivestl.com>
2020-04-15 20:29:49 -0500

    Group module wire

--- api/src/database/entity/group/group.wire.ts
+++ api/src/database/entity/group/group.wire.ts
@@ -1,7 +1,16 @@
+import * as Moment from 'moment';
+import { userWire } from '../user';
 import { GroupEntity } from './group.entity';
+import { Group } from 'fashionkilla-interfaces';
 
 export function groupWire(groupEntity: GroupEntity): Group {
   return {
-
+    id: groupEntity.id!,
+    name: groupEntity.name,
+    desc: groupEntity.description,
+    user: userWire(groupEntity.user!),
+    badge: groupEntity.badge,
+    roomID: groupEntity.roomID,
+    dateCreated: Moment.unix(groupEntity.dateCreated).toISOString(),
   }
 }
