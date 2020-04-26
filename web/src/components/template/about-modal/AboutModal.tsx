2347933ad1ff1273cdcb19fa71b1acd6bb770b1a
chrismpettyjohn <cpettyjohn@drivestl.com>
2020-04-26 03:07:08 -0500

    clean up about modal

--- web/src/components/template/about-modal/AboutModal.tsx
+++ web/src/components/template/about-modal/AboutModal.tsx
@@ -12,12 +12,11 @@ export function AboutModal({ isOpen, onToggle }: AboutModalProps) {
           <p>High performance, scalable content management system developed with Typescript, React and Nest.</p>
           <h5>Version</h5>
           <p>{healthContext.systemVersion}</p>
-          <h5>Built By</h5>
+          <h5>Developed By</h5>
           <p>LeChris</p>
-          <h5>Designed By</h5>
-          <p>Raizer</p>
           <h5>With Help By</h5>
           <ul>
+            <li>Raizer <small>(Original Layout)</small></li>
             <li>Layne <small>(Designed loading screen)</small></li>
           </ul>
         </div>
