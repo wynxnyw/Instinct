280facd2526d62780bfc08d70b5b830b0fe00e69
chrismpettyjohn <cpettyjohn@drivestl.com>
2020-05-09 15:41:54 -0500

    Change enter hotel to site name

--- examples/bobbarp/web/src/pages/home/home/my-profile/MyProfile.tsx
+++ examples/bobbarp/web/src/pages/home/home/my-profile/MyProfile.tsx
@@ -1,16 +1,17 @@
 import './MyProfile.scss';
 import { Link } from 'react-router-dom';
 import React, { useContext } from 'react';
-import { Avatar, Icon } from 'instinct-frontend';
-import { SessionContext, SessionTypes, } from 'app/context';
+import { SessionContext } from 'app/context';
+import { Avatar, ConfigContext, Icon } from 'instinct-frontend';
 
 export function MyProfile() {
-  const { user }: SessionTypes = useContext<SessionTypes>(SessionContext);
+  const { user } = useContext(SessionContext);
+  const { siteName } = useContext(ConfigContext);
   return (
     <article className="default-section profile-section">
       <div className="background-image">
         <Link className="rounded-button white plain hotel-button enter-hotel" to="/play">
-          Enter Hotel
+          Enter {siteName}
         </Link>
       </div>
       <div className="profile-content">
