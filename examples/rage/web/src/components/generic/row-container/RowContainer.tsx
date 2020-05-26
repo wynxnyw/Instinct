d16d6783bd1fc7babfcbaa5ca6a6dfc5d938750b
chrismpettyjohn <cpettyjohn@drivestl.com>
2020-05-25 20:28:53 -0500

    Various design improvements

--- examples/rage/web/src/components/generic/row-container/RowContainer.tsx
+++ examples/rage/web/src/components/generic/row-container/RowContainer.tsx
@@ -4,7 +4,7 @@ import { RowContainerProps } from './';
 
 export function RowContainer({ children, header, image, users, style }: RowContainerProps) {
   return (
-    <div className="row-container" style={{ background: `url(${image}) 10px no-repeat`, ...style }}>
+    <div className="row-container" style={{ backgroundImage: `url(${image})`, ...style }}>
       <div className="user-count">{users}</div>
       <div className="name">{header}</div>
       <div className="desc">{children}</div>
