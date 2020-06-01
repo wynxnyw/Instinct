f8e209cb44aa68b03b6c56548cc9ce418237947c
chrismpettyjohn <cpettyjohn@drivestl.com>
2020-05-31 22:26:40 -0500

    Style improvements

--- examples/rage/web/src/pages/home/user-profile/youtube-video/YoutubeVideo.tsx
+++ examples/rage/web/src/pages/home/user-profile/youtube-video/YoutubeVideo.tsx
@@ -5,14 +5,16 @@ import { UserProfileWidgetProps } from '../UserProfile.types';
 export function YoutubeVideo({ profile }: UserProfileWidgetProps) {
   return (
     <Card header="My Video">
-      <iframe
-        width={560}
-        height={315}
-        src={`https://www.youtube-nocookie.com/embed/${profile?.user?.youtube}?autoplay=true&modestbranding=1&showinfo=0&controls=0&loop=1`}
-        frameBorder={0}
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+      <div style={{ padding: 0, marginLeft: -20 }}>
+        <iframe
+          width={692}
+          height={315}
+          src={`https://www.youtube-nocookie.com/embed/${profile?.user?.youtube}?autoplay=true&modestbranding=1&showinfo=0&controls=0&loop=1`}
+          frameBorder={0}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
     </Card>
   );
 }
