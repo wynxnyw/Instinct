60c050097a09f8b2c9f683c109b2a486a0cff94e
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-10-08 03:22:34 -0500

    Add maintenance mode

--- libraries/interface/src/health/Health.ts
+++ libraries/interface/src/health/Health.ts
@@ -2,10 +2,12 @@ export interface Health {
   usersOnline: number;
   roomsActive: number;
   systemVersion: string;
+  maintenanceMode: boolean;
 }
 
 export const exampleHealth: Health = {
   usersOnline: 0,
   roomsActive: 0,
   systemVersion: '0.0.0',
+  maintenanceMode: false,
 }
