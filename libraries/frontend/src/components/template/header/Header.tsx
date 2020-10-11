b5eef69e925fc8b49ab907a3b3a5f31f8a58d755
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-10-11 18:45:02 -0500

    header ux improvements

--- libraries/frontend/src/components/template/header/Header.tsx
+++ libraries/frontend/src/components/template/header/Header.tsx
@@ -4,9 +4,12 @@ import { HeaderProps } from './';
 export function Header({ children }: HeaderProps) {
   return (
     <header id="header" className="header-container pixelated is-small is-logged">
-      <div className="header-content" style={{ width: '100%', maxWidth: 1055, margin: '0 auto' }}>
-        <div className="account-container">
-          <div id="account-buttons" className="account-buttons">
+      <div className="header-content row" style={{ width: '100%', maxWidth: 1055, margin: '0 auto', paddingTop: 0 }}>
+        <div className="col-6 text-left">
+          <img src="/img/logo/regular.png" style={{ marginLeft: -25 }} />
+        </div>
+        <div className="col-6 text-right">
+          <div id="account-buttons" className="account-buttons" style={{ marginTop: 10 }}>
             {children}
           </div>
         </div>
