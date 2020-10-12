512b3b4b2b15366225672306ac692b2d98e38318
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-10-11 19:05:00 -0500

    things

--- libraries/frontend/src/components/generic/jumbotron/Jumbotron.tsx
+++ libraries/frontend/src/components/generic/jumbotron/Jumbotron.tsx
@@ -5,7 +5,7 @@ import { JumbotronProps } from './';
 export function Jumbotron({ className = '', children, style, title }: JumbotronProps) {
   return (
     <header className={`page-header flex-container flex-vertical-center ${className}`} style={style}>
-      <div className="container">
+      <div className="container" style={{ maxWidth: 1055 }}>
         <div className="page-header-content">
           {title && <h1>{title}</h1>}
           {children}
