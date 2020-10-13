abe5526b007195908f0d6e952f42a31d8f15e88d
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-10-12 22:09:54 -0500

    add skeleton loading to staff

--- examples/habplex/web/src/pages/community/staff/Staff.tsx
+++ examples/habplex/web/src/pages/community/staff/Staff.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Container, Column,Jumbotron, Row, Loading, UserContainer, UserLayout, setURL, useFetchStaffTeam} from 'instinct-frontend';
+import { Card, Container, Column,Jumbotron, Row, UserContainer, UserLayout, setURL, Skeleton, useFetchStaffTeam} from 'instinct-frontend';
 
 setURL('community/staff', <Staff />);
 
@@ -12,23 +12,42 @@ export function Staff() {
         <p>These volunteer players represent the official team and are responsible for maintaining order and security inside the hotel.</p>
       </Jumbotron>
       <Container>
-        <Loading isLoading={staff === undefined}>
-          <Row>
-            <Column side="left">
-              <div className="members-container">
-                {staff?.map(rank => (
-                  <Card key={rank.id} header={rank.name}>
+        <Row>
+          <Column side="left">
+            <div className="members-container">
+              {
+                staff !== undefined
+                  ?  staff!.map(rank => (
+                    <Card key={rank.id} header={rank.name}>
                       {rank.users!.map(user => (
                         <div key={user.id} className="mt-2">
-                         <UserContainer user={user} />
+                          <UserContainer user={user} />
                         </div>
                       ))}
-                  </Card>
-                ))}
-              </div>
-            </Column>
-          </Row>
-        </Loading>
+                    </Card>
+                  ))
+                  : (
+                    <>
+                      <Card header="Staff">
+                        <div className="row">
+                          <div className="col-2">
+                            <Skeleton circle height={100} width={100}/>
+                          </div>
+                          <div className="col-8">
+                            <div className="mt-3">
+                              <Skeleton width={200} height={20} />
+                              <br/>
+                              <Skeleton width={200} height={20} />
+                            </div>
+                          </div>
+                        </div>
+                      </Card>
+                    </>
+                  )
+              }
+            </div>
+          </Column>
+        </Row>
       </Container>
     </UserLayout>
   );
