25ca003a30d1ea7a0e184d0870d220e8ebe4cb66
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-11-06 19:27:54 -0600

    Improved Forgot Password

--- packages/themes/src/roleplay/pages/auth/forgot-password/ForgotPassword.tsx
+++ packages/themes/src/roleplay/pages/auth/forgot-password/ForgotPassword.tsx
@@ -1,15 +1,85 @@
-import React from 'react';
-import {useLocation} from 'wouter';
+import {Link} from 'wouter';
+import {FormGroup} from 'reactstrap';
+import React, {useState} from 'react';
 import {GuestLayout} from '../../../components/layout/guest';
-import {ForgotPasswordModal, setURL} from '@instinct-prj/frontend';
+import {
+  defaultForgotPasswordModalState,
+  ForgotPasswordModalState,
+  Form,
+  Icon,
+  Input,
+  sessionService,
+  setURL,
+} from '@instinct-prj/frontend';
 
 setURL('forgot-password', <ForgotPassword />);
 
 export function ForgotPassword() {
-  const [location, setLocation] = useLocation();
+  const [state, setState] = useState<ForgotPasswordModalState>(
+    defaultForgotPasswordModalState
+  );
+
+  async function onSubmit() {
+    try {
+      setState(_ => ({
+        ..._,
+        showError: false,
+        showSpinner: true,
+      }));
+
+      await sessionService.generateForgotPasswordToken(state.email);
+      setState(_ => ({..._, showCompletion: true}));
+    } catch {
+      setState(_ => ({
+        ..._,
+        showError: true,
+        showSpinner: false,
+      }));
+    }
+  }
+
   return (
     <GuestLayout>
-      <ForgotPasswordModal isOpen onToggle={() => setLocation('/login')} />
+      <h1>Forgot Password?</h1>
+      {!state.showCompletion ? (
+        <Form className="" disabled={state.showSpinner} onSubmit={onSubmit}>
+          <p>Please enter your email address.</p>
+          <FormGroup>
+            <Input
+              type="email"
+              name="email"
+              value={state.email}
+              onChange={(key, email) => setState(_ => ({..._, email}))}
+            />
+          </FormGroup>
+          <FormGroup>
+            <div className="row">
+              <div className="col-6">
+                <Link className="forgot-password" to="/login">
+                  Back to Login
+                </Link>
+              </div>
+              <div className="col-6 text-right">
+                <button
+                  className="btn btn-success"
+                  disabled={state.showSpinner}
+                >
+                  {state.showSpinner ? (
+                    <Icon className="fa-spin" type="spinner" />
+                  ) : (
+                    <>Reset Password</>
+                  )}
+                </button>
+              </div>
+            </div>
+          </FormGroup>
+        </Form>
+      ) : (
+        <p>
+          Please check your inbox for an email containing your password reset
+          link.
+        </p>
+      )}
     </GuestLayout>
   );
 }
