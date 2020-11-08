e17bcaaeecdb015d43c0e6803aacf2a6d8509d23
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-11-07 19:02:16 -0600

    organize repo

--- apps/instinct-rp/web/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Jealousy} from './Jealousy';
-import '@instinct-prj/frontend/dist/frontend.css';
-import '@instinct-prj/admin/dist/admin.css';
-import '@instinct-prj/themes/dist/themes.css';
-
-ReactDOM.render(<Jealousy />, document.getElementById('root'));
