91b0f9da15d0d0e9936f8cb396973413161fc7a9
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-11-07 23:12:18 -0600

    fix paths

--- demo/roleplay/web/src/index.tsx
+++ demo/roleplay/web/src/index.tsx
@@ -1,6 +1,6 @@
 import '@instinct-prj/frontend/dist/frontend.css';
-import '@instinct-prj/themes/themes.css';
-import '@instinct-prj/admin/admin.css';
+import '@instinct-prj/themes/dist/themes.css';
+import '@instinct-prj/admin/dist/admin.css';
 import {bootstrapInstinctWebRP} from '@instinct-prj/rp-web/src';
 
 bootstrapInstinctWebRP();
