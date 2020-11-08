55139c1fc01ff9428f0b1bcf53b086ee1f865048
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2020-11-07 18:21:07 -0600

    demo apps

---
+++ apps/instinct-rp/api/src/app.module.ts
@@ -0,0 +1,31 @@
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {InstinctRPModule} from '@instinct-prj/backend-rp';
+import {CerberusModule} from '@instinct-prj/cerberus/src/cerberus.module';
+import {rpDatabaseEntities} from '@instinct-prj/backend-rp/src/database/database.meta';
+import {
+  databaseUser,
+  databaseName,
+  databaseHost,
+  databasePass,
+  databaseEntities,
+  InstinctModule,
+} from '@instinct-prj/backend';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: databaseHost,
+      username: databaseUser,
+      password: databasePass,
+      database: databaseName,
+      entities: [...databaseEntities, ...rpDatabaseEntities],
+      synchronize: false,
+    }),
+    CerberusModule,
+    InstinctModule,
+    InstinctRPModule,
+  ],
+})
+export class InstinctAppModule {}
---
+++ apps/instinct-rp/web/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Jealousy} from './Jealousy';
+import '@instinct-prj/frontend/dist/frontend.css';
+import '@instinct-prj/admin/dist/admin.css';
+import '@instinct-prj/themes/dist/themes.css';
+
+ReactDOM.render(<Jealousy />, document.getElementById('root'));
